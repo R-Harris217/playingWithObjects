75b1081eeed890680599150f5b36728e43a3f27f
Rob <rwr217@gmail.com>
2021-02-28 18:16:11 -0600

    first commit

---
+++ playingWithObjects.js
@@ -0,0 +1,6 @@
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+console.log(users[1].age)
+console.log(users[0])
+for(i=0; i<=users.length-1;i++){
+    console.log(users[i])
+}
